565567471cfb82c046d72946b7c100315b9626bd
Scott Glajch <glajchs@gmail.com>
2018-04-12 17:10:57 -0400

    Ensure keybinds using Mousetrap aren't initialized until the window.Mousetrap variable exists.

--- .slack/plugin-keybind-newchat.js
+++ .slack/plugin-keybind-newchat.js
@@ -8,16 +8,37 @@ window.slackPluginList.push({
     loadFunction: function() {
         // TODO: TS.stars.toggleStarForActiveModel()
 
-        // The "jump to" dialog (for opening new or existing conversations by name, type-completion)
-        // TODO: mac key binds should consider "cmd" instead of control.  Maybe use Mousetrap here?
-        Mousetrap.bind(['ctrl+n'], function() {
-            TS.key_triggers.getFromCode(75).func();
-        });
-        // TODO: mac key binds should consider "cmd" instead of control.  Maybe use Mousetrap here?
-        Mousetrap.bind(['ctrl+e'], function() {
-            TS.key_triggers.getFromCode(75).func();
-            // TODO: I've made this the same as ctrl+n for now
-            // TS.ui.im_browser.start();
-        });
+        // TODO: figure out when to do this programatically, instead of using intervals
+        var mousetrapIntervalMaxTries = 100;
+        var mousetrapIntervalCounter = 0;
+        var mousetrapInitializedInterval = setInterval(function() {
+            try {
+                mousetrapIntervalCounter++;
+                if (mousetrapIntervalCounter > mousetrapIntervalMaxTries) {
+                    clearInterval(mousetrapInitializedInterval);
+                    return;
+                }
+                if (typeof window.Mousetrap === "function") {
+                    initializeMousetrap();
+                    clearInterval(mousetrapInitializedInterval);
+                }
+            } catch (exception) {
+                // no-op
+            }
+        }, 100);
+
+        function initializeMousetrap() {
+            // The "jump to" dialog (for opening new or existing conversations by name, type-completion)
+            // TODO: mac key binds should consider "cmd" instead of control.  Maybe use Mousetrap here?
+            Mousetrap.bind(['ctrl+n'], function() {
+                TS.key_triggers.getFromCode(75).func();
+            });
+            // TODO: mac key binds should consider "cmd" instead of control.  Maybe use Mousetrap here?
+            Mousetrap.bind(['ctrl+e'], function() {
+                TS.key_triggers.getFromCode(75).func();
+                // TODO: I've made this the same as ctrl+n for now
+                // TS.ui.im_browser.start();
+            });
+        }
     }
 });
